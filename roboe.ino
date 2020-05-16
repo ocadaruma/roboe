fcc5038bd9f2c4dd84a58a0c96ac96cc770a6ceb
ocadaruma <ocadaruma@gmail.com>
2020-05-16 17:41:45 +0900

    photo reflector

--- roboe.ino
+++ roboe.ino
@@ -4,6 +4,9 @@
 #include "musical.h"
 #include "oboe.h"
 
+const unsigned int REFLECTOR_CAPACITOR_DELAY_US = 12;
+const unsigned long REFLECTOR_TIMEOUT_US = 1000;
+const int REFLECTOR_PIN = 11;
 const int SENSOR_PIN = A1;
 const int SELECTOR_NUM = 5;
 const int INPUT_NUM = 5;
@@ -15,8 +18,8 @@ const int INPUT_PIN[INPUT_NUM] = {0, 10, 2, 3, 4};
 const int BREATH_MAX_READING = 1014;
 
 const OboeKey OBOE_KEY_MAP[SELECTOR_NUM][INPUT_NUM] = {
-        { OboeKey::LEFT_POINTER     , OboeKey::LEFT_RING          , OboeKey::LEFT_LITTLE_F        , OboeKey::RIGHT_RING_F        , OboeKey::LEFT_THUMB_83   , },
-        { OboeKey::LEFT_POINTER_HALF, OboeKey::LEFT_LITTLE_G_SHARP, OboeKey::RIGHT_POINTER_G_SHARP, OboeKey::RIGHT_RING          , OboeKey::LEFT_THUMB_81   , },
+        { OboeKey::UNKNOWN          , OboeKey::LEFT_RING          , OboeKey::LEFT_LITTLE_F        , OboeKey::RIGHT_RING_F        , OboeKey::LEFT_THUMB_83   , },
+        { OboeKey::LEFT_POINTER     , OboeKey::LEFT_LITTLE_G_SHARP, OboeKey::RIGHT_POINTER_G_SHARP, OboeKey::RIGHT_RING          , OboeKey::LEFT_THUMB_81   , },
         { OboeKey::LEFT_MIDDLE_TR_1 , OboeKey::LEFT_LITTLE_B      , OboeKey::RIGHT_POINTER        , OboeKey::RIGHT_LITTLE_C      , OboeKey::LEFT_POINTER_82 , },
         { OboeKey::LEFT_MIDDLE      , OboeKey::LEFT_LITTLE_B_FLAT , OboeKey::RIGHT_MIDDLE_TR_3    , OboeKey::RIGHT_LITTLE_C_SHARP, OboeKey::UNKNOWN, },
         { OboeKey::LEFT_RING_TR_2   , OboeKey::LEFT_LITTLE_E_FLAT , OboeKey::RIGHT_MIDDLE         , OboeKey::RIGHT_LITTLE_E_FLAT , OboeKey::UNKNOWN, },
@@ -28,10 +31,10 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 Roboe ROBOE(MIDI);
 
 void setup() {
+    delay(200);
+
     MIDI.begin(1);
     MIDI.turnThruOff();
-
-    delay(200);
 
     // initialize breath controller
     BREATH_CALIBRATION = analogRead(SENSOR_PIN);
@@ -49,6 +52,22 @@ void setup() {
 }
 
 void loop() {
+    // photo reflector (half open) part
+    pinMode(REFLECTOR_PIN, OUTPUT);
+    digitalWrite(REFLECTOR_PIN, HIGH);
+    // wait capacitor to be charged
+    delayMicroseconds(REFLECTOR_CAPACITOR_DELAY_US);
+    pinMode(REFLECTOR_PIN, INPUT);
+
+    unsigned long t0 = micros();
+    bool halfOpenPressed = false;
+    while (micros() - t0 < REFLECTOR_TIMEOUT_US) {
+        if (digitalRead(REFLECTOR_PIN) == LOW) {
+            halfOpenPressed = true;
+            break;
+        }
+    }
+
     OboeKeys keyCombination;
 
     // keys part
@@ -63,10 +82,23 @@ void loop() {
             }
 
             int keyReading = digitalRead(INPUT_PIN[j]);
-            if (keyReading == LOW) {
-                keyCombination = keyCombination | oboeKey;
+
+            if (oboeKey == OboeKey::LEFT_POINTER) {
+                if (keyReading == LOW) {
+                    if (halfOpenPressed) {
+                        keyCombination = keyCombination | OboeKey::LEFT_POINTER;
+                    } else {
+                        keyCombination = (keyCombination - OboeKey::LEFT_POINTER) | OboeKey::LEFT_POINTER_HALF;
+                    }
+                } else {
+                    keyCombination = (keyCombination - OboeKey::LEFT_POINTER) - OboeKey::LEFT_POINTER_HALF;
+                }
             } else {
-                keyCombination = keyCombination - oboeKey;
+                if (keyReading == LOW) {
+                    keyCombination = keyCombination | oboeKey;
+                } else {
+                    keyCombination = keyCombination - oboeKey;
+                }
             }
         }
 
